b0554544365a4031ce99495597215f48b76ac188
Otavio Ferreira <38397566+otavio-f@users.noreply.github.com>
2024-11-04 09:47:34 -0300

    feat: Adiciona ArrayQueue Adiciona ArrayQueue e testes

---
+++ src/main/java/br/otaviof/czech_accidents/dataStructures/ArrayQueue.java
@@ -0,0 +1,38 @@
+package br.otaviof.czech_accidents.dataStructures;
+
+public class ArrayQueue<T extends Comparable<? super T>> implements Queue<T> {
+
+    public ArrayQueue(int size) {
+
+    }
+
+    @Override
+    public void enqueue(T item) {
+
+    }
+
+    @Override
+    public T dequeue() {
+        return null;
+    }
+
+    @Override
+    public T peekTail() {
+        return null;
+    }
+
+    @Override
+    public T peekHead() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+}
