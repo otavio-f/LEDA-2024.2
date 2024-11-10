8476996f9354dd873f2fd08449079a75e50f6e34
Otavio Ferreira <38397566+otavio-f@users.noreply.github.com>
2024-11-10 14:21:47 -0300

    docs: Documentação da interface pilha

--- src/main/java/br/otaviof/czech_accidents/adt/stack/Stack.java
+++ src/main/java/br/otaviof/czech_accidents/adt/stack/Stack.java
@@ -1,14 +1,36 @@
 package br.otaviof.czech_accidents.adt.stack;
 
+/**
+ * Interface para classes de pilha, tipo Last Insert Firtst Out
+ * @param <T> Um tipo genérico comparável
+ * @author otavio-f
+ */
 public interface Stack <T extends Comparable<? super T>> {
 
-    public void push(T item);
+    /**
+     * Adiciona um elemento no topo da pilha
+     * @param item
+     */
+    void push(T item);
 
-    public T pop();
+    /**
+     * Remove um elemento da pilha
+     * @return O elemento removido
+     */
+    T pop();
 
-    public T top();
+    /**
+     * Verifica qual elemento está no topo da pilha
+     * @return O elemento no topo
+     */
+    T top();
 
-    public Stack<T> multitop(int k);
+    // TODO: Entender como funciona isso
+    Stack<T> multitop(int k);
 
-    public boolean isEmpty();
+    /**
+     * Verifica se a pilha não contém itens
+     * @return true se a pilha não contém nenhum item, senão false
+     */
+    boolean isEmpty();
 }
