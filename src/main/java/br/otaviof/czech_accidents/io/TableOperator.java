883f078bbb05e22583184aa09386f848bc38a31c
Otavio Ferreira <38397566+otavio-f@users.noreply.github.com>
2024-11-19 19:52:02 -0300

    refactor: Move operações para TableOperator

---
+++ src/main/java/br/otaviof/czech_accidents/io/TableOperator.java
@@ -0,0 +1,103 @@
+package br.otaviof.czech_accidents.io;
+
+import br.otaviof.czech_accidents.adt.list.CustomList;
+import br.otaviof.czech_accidents.adt.list.LinkedList;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Classe leitora de tabela
+ * @author otavio-f
+ */
+public class TableOperator {
+
+    /**
+     * Padrão de uma célula de um arquivo separado por vírgulas
+     * Referência: <a href="https://regex101.com/library/eH1zP0">...</a>
+     */
+    final private static Pattern CELL_REGEX = Pattern.compile("(?:\\s*(?:\\\"([^\\\"]*)\\\"|([^,]*))\\s*,?)+?", Pattern.CASE_INSENSITIVE);
+
+    private final File input;
+    private CustomList<String> headers;
+
+    protected TableOperator(String file) {
+        this.input = new File(file);
+    }
+
+    protected CustomList<String> getHeaders() {
+        return this.headers;
+    }
+
+    /**
+     * Cria um BufferedReader no arquivo fonte
+     * @return Uma instância de BufferedReader
+     * @throws IOException Se ocorrer erro de leitura
+     */
+    protected BufferedReader getReader() throws IOException {
+        FileReader fr = new FileReader(this.input);
+        return new BufferedReader(fr);
+    }
+
+    /**
+     * Separa a linha de cabeçalho em cabeçalhos
+     * @param line A primeira linha
+     */
+    protected void readHeaders(String line) {
+        Matcher matcher = CELL_REGEX.matcher(line);
+
+        CustomList<String> result = new LinkedList<>();
+        while (matcher.find()) {
+            String cell = (matcher.group(1) != null) ? matcher.group(1) : matcher.group(2);
+            result.append(cell);
+        }
+
+        this.headers = result;
+    }
+
+    /**
+     * Parte uma linha em células
+     * @param line A linha do arquivo tabular
+     * @param cellCount A quantidade de células a ser extraída
+     * @param fill O que colocar caso não possua células suficientes
+     * @return Uma lista com as células
+     */
+    protected CustomList<String> splitLine(String line, int cellCount, String fill) {
+        Matcher matcher = CELL_REGEX.matcher(line);
+        CustomList<String> result = new LinkedList<>();
+
+        for(int i=0; i<cellCount; i++) {
+            if(!matcher.find()) {
+                result.append(fill);
+                continue;
+            }
+            String cell = (matcher.group(1) != null)? matcher.group(1) : matcher.group(2);
+            result.append(cell);
+        }
+
+        return result;
+    }
+
+    /**
+     * Encontra o índice da coluna que possui o cabeçalho
+     * @param headerName O nome do cabeçalho
+     * @return O índice da coluna
+     * @throws IllegalArgumentException se o nome do cabeçalho não estiver contido na primeira linha
+     */
+    protected int findColumnIndex(String headerName) {
+        int count = 0;
+        Iterator<String> iter = this.headers.getIterator();
+        while(iter.hasNext()) {
+            if(Objects.equals(iter.next(), headerName))
+                return count;
+            count++;
+        }
+        throw new IllegalArgumentException(String.format("Column %s not found!", headerName));
+    }
+}
--- src/main/java/br/otaviof/czech_accidents/io/TableReader.java
+++ src/main/java/br/otaviof/czech_accidents/io/TableReader.java
@@ -4,13 +4,8 @@ import br.otaviof.czech_accidents.adt.list.CustomList;
 import br.otaviof.czech_accidents.adt.list.LinkedList;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Classe leitora de tabela
@@ -18,74 +13,10 @@ import java.util.regex.Pattern;
  */
 public class TableReader {
 
-    /**
-     * Padrão de uma célula de um arquivo separado por vírgulas
-     * Referência: <a href="https://regex101.com/library/eH1zP0">...</a>
-     */
-    final private static Pattern CELL_REGEX = Pattern.compile("(?:\\s*(?:\\\"([^\\\"]*)\\\"|([^,]*))\\s*,?)+?", Pattern.CASE_INSENSITIVE);
-
-    private final File input;
-    private CustomList<String> headers;
+    private final TableOperator operator;
 
     public TableReader(String file) {
-        this.input = new File(file);
-    }
-
-    /**
-     * Separa a linha de cabeçalho em cabeçalhos
-     * @param line A primeira linha
-     * @return Uma lista com cabeçalhos separados
-     */
-    private CustomList<String> readHeaders(String line) {
-        Matcher matcher = CELL_REGEX.matcher(line);
-
-        CustomList<String> result = new LinkedList<>();
-        while (matcher.find()) {
-            String cell = (matcher.group(1) != null) ? matcher.group(1) : matcher.group(2);
-            result.append(cell);
-        }
-
-        return result;
-    }
-
-    /**
-     * Parte uma linha em células
-     * @param line A linha do arquivo tabular
-     * @param cellCount A quantidade de células a ser extraída
-     * @param fill O que colocar caso não possua células suficientes
-     * @return Uma lista com as células
-     */
-    private CustomList<String> splitLine(String line, int cellCount, String fill) {
-        Matcher matcher = CELL_REGEX.matcher(line);
-        CustomList<String> result = new LinkedList<>();
-
-        for(int i=0; i<cellCount; i++) {
-            if(!matcher.find()) {
-                result.append(fill);
-                continue;
-            }
-            String cell = (matcher.group(1) != null)? matcher.group(1) : matcher.group(2);
-            result.append(cell);
-        }
-
-        return result;
-    }
-
-    /**
-     * Encontra o índice da coluna que possui o cabeçalho
-     * @param headerName O nome do cabeçalho
-     * @return O índice da coluna
-     * @throws IllegalArgumentException se o nome do cabeçalho não estiver contido na primeira linha
-     */
-    private int findColumnIndex(String headerName) {
-        int count = 0;
-        Iterator<String> iter = this.headers.getIterator();
-        while(iter.hasNext()) {
-            if(Objects.equals(iter.next(), headerName))
-                return count;
-            count++;
-        }
-        throw new IllegalArgumentException(String.format("Column %s not found!", headerName));
+        this.operator = new TableOperator(file);
     }
 
     /**
@@ -97,27 +28,26 @@ public class TableReader {
      * @throws IOException Se ocorrer algum erro de leitura
      */
     public <T extends Comparable<? super T>> CustomList<T> getColumnAs(String name, CellConverter<T> converter) throws IOException {
-        FileReader fr = new FileReader(this.input);
-        BufferedReader br = new BufferedReader(fr);
+        BufferedReader br = this.operator.getReader();
         String line = br.readLine();
 
-        this.headers = this.readHeaders(line);
-        int columnCount = this.headers.getSize();
+        this.operator.readHeaders(line);
 
-        final int index = findColumnIndex(name);
+        int columnCount = this.operator.getHeaders().getSize();
+
+        final int index = this.operator.findColumnIndex(name);
 
         CustomList<T> result = new LinkedList<>();
         line = br.readLine();
 
         while(line != null) {
-            CustomList<String> cells = splitLine(line, columnCount, "");
+            CustomList<String> cells = this.operator.splitLine(line, columnCount, "");
             T converted = converter.convert(cells.getAt(index));
             result.append(converted);
             line = br.readLine();
         }
 
         br.close();
-        fr.close();
 
         return result;
     }
